6c7f22a2aa3dc6fc20ff39151be992f86c2f0254
Nikisha <np05cp4a220078@iic.edu.np>
2024-12-18 16:03:33 +0545

    first commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace WalletManager
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace WalletManager
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
